a55b4026c5130d26d6e6f73fe01be67f3e8e593b
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-05-14 19:31:40 +0200

    Angry ants and tameable ants

--- src/main/java/com/ninni/arthropoda/entity/AntEntity.java
+++ src/main/java/com/ninni/arthropoda/entity/AntEntity.java
@@ -4,26 +4,202 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.goal.ActiveTargetGoal;
+import net.minecraft.entity.ai.goal.AttackWithOwnerGoal;
+import net.minecraft.entity.ai.goal.EscapeDangerGoal;
+import net.minecraft.entity.ai.goal.FollowOwnerGoal;
+import net.minecraft.entity.ai.goal.LookAroundGoal;
+import net.minecraft.entity.ai.goal.LookAtEntityGoal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.PounceAtTargetGoal;
+import net.minecraft.entity.ai.goal.RevengeGoal;
+import net.minecraft.entity.ai.goal.SitGoal;
+import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.ai.goal.TemptGoal;
+import net.minecraft.entity.ai.goal.TrackOwnerAttackerGoal;
+import net.minecraft.entity.ai.goal.UniversalAngerGoal;
+import net.minecraft.entity.ai.goal.UntamedActiveTargetGoal;
+import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
+import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.mob.Angerable;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.passive.TurtleEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TimeHelper;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
 import java.util.Random;
+import java.util.UUID;
 
-public class AntEntity extends AnimalEntity {
-    protected AntEntity(EntityType<? extends AnimalEntity> entityType, World world) { super(entityType, world); }
+public class AntEntity extends TameableEntity implements Angerable {
+    private static final TrackedData<Integer> ANGER_TIME = DataTracker.registerData(AntEntity.class, TrackedDataHandlerRegistry.INTEGER);
+    private UUID angryAt;
+    private static final UniformIntProvider ANGER_TIME_RANGE = TimeHelper.betweenSeconds(20, 39);
+
+    protected AntEntity(EntityType<? extends TameableEntity> entityType, World world) {
+        super(entityType, world);
+        this.setPathfindingPenalty(PathNodeType.WATER, 1);
+    }
+
+    @Override
+    protected void initGoals() {
+        this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(1, new EscapeDangerGoal(this, 1.25));
+        this.goalSelector.add(2, new SitGoal(this));
+        this.goalSelector.add(3, new MeleeAttackGoal(this, 1.25F, true));
+        this.goalSelector.add(2, new PounceAtTargetGoal(this, 0.2F));
+        this.goalSelector.add(4, new FollowOwnerGoal(this, 1.0, 10.0F, 2.0F, true));
+        this.goalSelector.add(4, new TemptGoal(this, 1.25, Ingredient.ofItems(Items.SUGAR), false));
+        this.goalSelector.add(5, new WanderAroundFarGoal(this, 0.8));
+        this.goalSelector.add(6, new LookAroundGoal(this));
+        this.goalSelector.add(7, new LookAtEntityGoal(this, PlayerEntity.class, 6));
+
+        this.targetSelector.add(1, new TrackOwnerAttackerGoal(this));
+        this.targetSelector.add(2, new AttackWithOwnerGoal(this));
+        this.targetSelector.add(3, (new RevengeGoal(this)).setGroupRevenge());
+        this.targetSelector.add(4, new ActiveTargetGoal<>(this, PlayerEntity.class, 10, true, false, this::shouldAngerAt));
+        this.targetSelector.add(6, new UntamedActiveTargetGoal<>(this, TurtleEntity.class, false, TurtleEntity.BABY_TURTLE_ON_LAND_FILTER));
+        this.targetSelector.add(8, new UniversalAngerGoal<>(this, true));
+    }
 
     public static DefaultAttributeContainer.Builder createAntAttributes() {
         return createMobAttributes()
-            .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.2)
-            .add(EntityAttributes.GENERIC_MAX_HEALTH, 3.0D)
-            .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2.0D);
+            .add(EntityAttributes.GENERIC_MAX_HEALTH, getRandomHealth())
+            .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, getRandomMovementSpeed())
+            .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, getRandomAttackDamage());
+    }
+    protected static float getRandomHealth() { return 2.0F; }
+    protected static double getRandomMovementSpeed() { return 0.2; }
+    protected static double getRandomAttackDamage() { return 1; }
+
+    @Override
+    protected void initDataTracker() {
+        super.initDataTracker();
+        this.dataTracker.startTracking(ANGER_TIME, 0);
+    }
+
+    @Override
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+        this.writeAngerToNbt(nbt);
+    }
+
+    @Override
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
+        this.readAngerFromNbt(this.world, nbt);
+    }
+
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        Item item = itemStack.getItem();
+        if (this.world.isClient) {
+            boolean bl = this.isOwner(player) || this.isTamed() || item == Items.SUGAR && !this.isTamed();
+            return bl ? ActionResult.CONSUME : ActionResult.PASS;
+        } else {
+            if (this.isTamed()) {
+                if (this.isHealingItem(itemStack) && this.getHealth() < this.getMaxHealth()) {
+                    if (!this.isSilent()) {
+                        this.world.playSoundFromEntity(null, this, SoundEvents.ENTITY_GENERIC_EAT, this.getSoundCategory(), 1.0F, 1.0F + (this.random.nextFloat() - this.random.nextFloat()) * 0.2F);
+                    }
+                    if (!player.getAbilities().creativeMode) {
+                        itemStack.decrement(1);
+                    }
+
+                    this.heal((float)item.getFoodComponent().getHunger());
+                    return ActionResult.SUCCESS;
+                }
+                ActionResult actionResult = super.interactMob(player, hand);
+                if ((!actionResult.isAccepted() || this.isBaby()) && this.isOwner(player)) {
+                    this.setSitting(!this.isSitting());
+                    this.jumping = false;
+                    this.navigation.stop();
+                    this.setTarget(null);
+                    return ActionResult.SUCCESS;
+                }
+
+                return actionResult;
+
+
+            } else if (item == Items.SUGAR) {
+                if (!this.isSilent()) {
+                    this.world.playSoundFromEntity(null, this, SoundEvents.ENTITY_GENERIC_EAT, this.getSoundCategory(), 1.0F, 1.0F + (this.random.nextFloat() - this.random.nextFloat()) * 0.2F);
+                }
+                if (!player.getAbilities().creativeMode) {
+                    itemStack.decrement(1);
+                }
+
+                if (this.random.nextInt(3) == 0) {
+                    this.setOwner(player);
+                    this.navigation.stop();
+                    this.setTarget(null);
+                    this.setSitting(true);
+                    this.world.sendEntityStatus(this, (byte)7);
+                } else {
+                    this.world.sendEntityStatus(this, (byte)6);
+                }
+
+                return ActionResult.SUCCESS;
+            }
+
+            return super.interactMob(player, hand);
+        }
+    }
+
+    public boolean isHealingItem (ItemStack stack){
+        Item item = stack.getItem();
+        return item.isFood() && !Objects.requireNonNull(item.getFoodComponent()).isMeat();
+    }
+
+    @Override
+    public boolean handleFallDamage(float fallDistance, float damageMultiplier, DamageSource damageSource) { return false; }
+    @Override
+    public boolean hurtByWater() { return true; }
+
+    @Override
+    public int getAngerTime() {
+        return this.dataTracker.get(ANGER_TIME);
+    }
+    @Override
+    public void setAngerTime(int angerTime) {
+        this.dataTracker.set(ANGER_TIME, angerTime);
+    }
+    @Override
+    public void chooseRandomAngerTime() {
+        this.setAngerTime(ANGER_TIME_RANGE.get(this.random));
+    }
+    @Override
+    @Nullable
+    public UUID getAngryAt() {
+        return this.angryAt;
+    }
+    @Override
+    public void setAngryAt(@Nullable UUID angryAt) {
+        this.angryAt = angryAt;
     }
 
     @Nullable
